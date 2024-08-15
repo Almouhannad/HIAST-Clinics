8150215ac0b5f163c0894bb516f734903c868bba
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-15 22:20:51 +0300

    Add Clinics database context

---
+++ Clinics.Backend/Persistence/Context/ClinicsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Context;
+
+public partial class ClinicsDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // TODO: Use options pattern
+        optionsBuilder.UseSqlServer("server=.\\;database=Clinics_Test;Trusted_Connection=True; Encrypt=False;MultipleActiveResultSets=true");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClinicsDbContext).Assembly);
+    }
+}
