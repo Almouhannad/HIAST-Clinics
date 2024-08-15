ef4807f3bb91c6466d8765e1d0fc9236d8b17aee
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-15 18:43:52 +0300

    (B) Add waiting list recode entity

---
+++ Clinics.Backend/Domain/Entities/WaitingList/WaitingListRecord.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities.People.Doctors;
+using Domain.Entities.People.Patients;
+using Domain.Primitives;
+
+namespace Domain.Entities.WaitingList;
+
+public sealed class WaitingListRecord(int id) : Entity(id)
+{
+    #region Properties
+
+    #region Patient
+
+    public int PatientId { get; set; }
+    public Patient Patient { get; set; } = null!;
+
+    #endregion
+
+    #region Doctor
+
+    public int? DoctorId { get; set; }
+    public Doctor? Doctor { get; set; }
+
+    #endregion
+
+    #region Additional
+
+    public bool IsServed { get; set; } = false;
+
+    #endregion
+
+    #endregion
+}
