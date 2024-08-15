d8ab4f33a344b63f3c09d6a9588590155b77bbcc
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-15 18:42:21 +0300

    (B) Add Doctor entity

---
+++ Clinics.Backend/Domain/Entities/People/Doctors/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities.People.Doctors.Shared;
+using Domain.Entities.People.Doctors.Shared.Constants.DoctorStatusValues;
+using Domain.Entities.People.Shared;
+using Domain.Primitives;
+
+namespace Domain.Entities.People.Doctors;
+
+public sealed class Doctor(int id) : Entity(id)
+{
+    #region Properties
+
+    public PersonalInfo PersonalInfo { get; set; } = null!;
+
+    public DoctorStatus Status { get; set; } = null!;
+
+    #region Navigations
+
+    public ICollection<DoctorPhone> Phones { get; set; } = [];
+
+    #endregion
+
+    #endregion
+}
