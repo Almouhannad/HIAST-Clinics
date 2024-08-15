fa70fd5184bcf8c3ef403d4575e5f94509618cb4
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-16 01:11:30 +0300

    (B) Add test controller into presentation

---
+++ Clinics.Backend/Presentation/Controllers/TestsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            object respone = new { Result = "Hello world!" };
+            return Ok(respone);
+        }
+    }
+}
