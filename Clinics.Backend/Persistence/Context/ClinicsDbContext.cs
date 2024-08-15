8791ccbe18319d85d3ca77345fcb360a3bb5c819
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-15 22:56:35 +0300

    (B) Add first migration

--- Clinics.Backend/Persistence/Context/ClinicsDbContext.cs
+++ Clinics.Backend/Persistence/Context/ClinicsDbContext.cs
@@ -7,11 +7,11 @@ public partial class ClinicsDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // TODO: Use options pattern
-        optionsBuilder.UseSqlServer("server=.\\;database=Clinics_Test;Trusted_Connection=True; Encrypt=False;MultipleActiveResultSets=true");
+        optionsBuilder.UseSqlServer("server=.\\;database=Clinics;Trusted_Connection=True; Encrypt=False;MultipleActiveResultSets=true");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClinicsDbContext).Assembly);
+        //modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClinicsDbContext).Assembly);
     }
 }
