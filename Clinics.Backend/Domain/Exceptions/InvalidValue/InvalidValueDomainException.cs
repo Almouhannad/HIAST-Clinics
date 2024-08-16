a7ffc35bc4a4628ba277a35704defbc24296220b
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-16 13:51:01 +0300

    (B) Add domain exceptions

---
+++ Clinics.Backend/Domain/Exceptions/InvalidValue/InvalidValueDomainException.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Exceptions.Base;
+using Domain.Primitives;
+
+namespace Domain.Exceptions.InvalidValue;
+
+public class InvalidValueDomainException<TEntity> : DomainException
+    where TEntity : Entity
+{
+    private InvalidValueDomainException(string message = $"Values entered for entity {nameof(TEntity)} are invalid")
+        : base(message)
+    {
+    }
+}
