d42c6ffd983b4a2ca118dbbc99693e143ba8ce2f
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-16 12:45:07 +0300

    (B) Add base repositories

---
+++ Clinics.Backend/Domain/Repositories/Base/IRepository.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Repositories.Base
+{
+    // CRUD operations
+    public interface IRepository<TEntity>
+        where TEntity : Entity
+    {
+
+        #region Create operation
+
+        public Task<TEntity> CreateAsync(TEntity entity);
+
+        #endregion
+
+        #region Read operations
+
+        public Task<TEntity?> GetByIdAsync(int id);
+
+        public Task<ICollection<TEntity>> GetAllAsync();
+
+        #endregion
+
+        #region Update oprtation
+
+        public Task<TEntity> UpdateAsync(TEntity entity);
+
+        #endregion
+
+        #region Delete operation
+
+        public Task DeleteAsync(int id);
+
+        #endregion
+    }
+}
