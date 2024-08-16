964853eb0fcba255a8a93360243053cc90098d6b
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-16 18:55:20 +0300

    (B) Update family member entity

--- Clinics.Backend/Domain/Entities/People/FamilyMembers/FamilyMember.cs
+++ Clinics.Backend/Domain/Entities/People/FamilyMembers/FamilyMember.cs
@@ -3,11 +3,47 @@ using Domain.Primitives;
 
 namespace Domain.Entities.People.FamilyMembers;
 
-public sealed class FamilyMember(int id) : Entity(id)
+public sealed class FamilyMember : Entity
 {
+    #region Private ctor
+    private FamilyMember(int id) : base(id)
+    {
+    }
+
+    private FamilyMember(int id, Patient patient) : base(id)
+    {
+        Patient = patient;
+    }
+    #endregion
+
     #region Properties
 
     public Patient Patient { get; set; } = null!;
+
+    #endregion
+
+    #region Methods
+
+    #region Static factory
+    public static FamilyMember Create(string firstName, string middleName, string lastName,
+        DateOnly dateOfBirth,
+        string gender
+        )
+    {
+        Patient patient;
+
+        try
+        {
+            patient = Patient.Create(firstName, middleName, lastName, dateOfBirth, gender);
+        }
+        catch
+        {
+            throw;
+        }
+
+        return new FamilyMember(0, patient);
+    }
+    #endregion
 
     #endregion
 }
