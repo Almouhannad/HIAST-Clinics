4c0a6b164607e8a1087ced173585a12a851f5a53
Almouhannad <almouhannad.hafez@gmail.com>
2024-08-17 18:43:11 +0300

    (B) Add CQRS bases

---
+++ Clinics.Backend/Application/Abstractions/CQRS/Commands/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Application.Abstractions.CQRS.Commands;
+
+// No response
+public interface ICommandHandler<TCommand> : IRequestHandler<TCommand>
+    where TCommand : ICommand
+{
+}
+
+// With response
+public interface ICommandHandler<TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+{
+}
